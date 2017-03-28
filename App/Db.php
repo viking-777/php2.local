a9bad5edec4d2a52f303093a5b6c9671e7cb6e99
viking-777 <fgtesd@mail.ru>
2017-03-28 17:30:46 +0300

    таблица не отвечает

--- App/Db.php
+++ App/Db.php
@@ -6,9 +6,26 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
     public function __construct()
     {
-        echo 'Hello, Db';
+        $this->dbh = new \PDO('mysql:host = 127.0.0.1; dbname = test','root','');
     }
 
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth ->execute();
+        return $res;
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth ->execute();
+        if(false !== $res){
+            return $sth->fetchAll();
+        }
+        return [];
+    }
 }
